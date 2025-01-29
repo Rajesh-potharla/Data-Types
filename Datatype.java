a33b6a89bb19fd832e14c9b8ac9a8225cd9dd4d2
Rajesh-potharla <prajeshyadavraju@gmail.com>
2025-01-29 07:25:25 +0530

    Add files via upload

---
+++ Datatype.java
@@ -0,0 +1,17 @@
+public class Datatype {
+    public static void main(String[] args) {
+        int a=12;
+        float b =3.33f;
+        double c=2.443;
+        String d="hello";
+        char e='h';
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+     
+
+
+    }
+}
